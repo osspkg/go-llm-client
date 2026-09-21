7a03afd9d0dab83425d31f6624ecf4793dc79559
Mikhail Knyazhev <markus621@yandex.com>
2026-09-21 05:33:03 +0300

    change

--- openai/stateful/client.go
+++ openai/stateful/client.go
@@ -1,3 +1,8 @@
+/*
+ *  Copyright (c) 2026 Mikhail Knyazhev <markus621@yandex.com>. All rights reserved.
+ *  Use of this source code is governed by a BSD 3-Clause license that can be found in the LICENSE file.
+ */
+
 // Package stateful implements OpenAI assistant, thread, run, vector-store,
 // container, and evaluation resource operations.
 package stateful
